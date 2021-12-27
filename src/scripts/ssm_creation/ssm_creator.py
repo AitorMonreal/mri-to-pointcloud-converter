39782e5ee4906a1af6ad585ab798b14ff8bff5b9
Aitor <monreaitorcelay@gmail.com>
2021-12-27 13:09:13 +0000

    Added drawio diagram, and added arguments for argument parsing.

--- src/scripts/ssm_creation/ssm_creator.py
+++ src/scripts/ssm_creation/ssm_creator.py
@@ -21,16 +21,19 @@ import os
 # SYS
 import sys
 
+# NumPy
+import numpy as np
+
 
 class FemurSurface(object):
-    def __init__(self, data_dir: str, filename: str):
+    def __init__(self, data_dir: str, filename: str, downsampling_ratio: float):
         self.filename: str = filename
         self.femur_vtk_surface = self.__read_mhd_file(data_dir)
         self.smoothing_iterations: int = 14
         self.pass_band: float = 0.001
         self.feature_angle: float = 120.0
         self.femur_mesh = None
-        self.downsampling_ratio = 0.2
+        self.downsampling_ratio: float = downsampling_ratio
 
     def __read_mhd_file(self, data_dir: str):
         mhd_file_path: str = data_dir + "/" + self.filename
@@ -90,44 +93,49 @@ def __parse_arguments() -> argparse.Namespace:
 
     parser.add_argument("--segmented_data_dir", type=str, default='~/.config',
                         help="Directory where you are storing the 3D MRI segmented data as mhd (and raw) files.")
-    parser.add_argument("--visualise", type=bool, default='False', help="Do you want to visualise the surfaces, "
-                                                                        "True or False?")
+
+    parser.add_argument('--visualise', action='store_true', help="Do you want to visualise the surfaces, "
+                                                                            "True or False?")
+    parser.set_defaults(visualise=False)
+    parser.add_argument("--pointcloud_data_dir", type=str, default='~/.config',
+                        help="Directory where you are storing the produced pointcloud files.")
+    parser.add_argument("--downsampling_ratio", type=float, default='0.25',
+                        help="Femur mesh downsampling ratio, from 0 to 1, where 1 equates to no downsampling.")
 
     argcomplete.autocomplete(parser)
+    args = parser.parse_args()
 
-    return parser.parse_args()
+    if args.downsampling_ratio <= 0.0 or args.downsampling_ratio > 1.0:
+        raise argparse.ArgumentTypeError("Mesh downsampling ratio must be a float larger than 0 and no bigger than 1.")
+
+    return args
 
 
-def load_data(data_dir: str):
-    load_femur_bone(data_dir)
+def load_data(data_dir: str, visualise: bool, pointcloud_dir: str, downsampling_ratio: float):
+    load_femur_bone(data_dir, visualise, pointcloud_dir, downsampling_ratio)
     # load_tibia_bone()
     # load_femur_cartilage()
     # load_tibia_cartilage()
 
 
-def load_femur_bone(data_dir: str, visualise: bool, pointcloud_dir: str):
+def load_femur_bone(data_dir: str, visualise: bool, pointcloud_dir: str, downsampling_ratio: float):
     count: int = 0
     file_header: str = "femur_bone_"
 
     for filename in os.listdir(data_dir):
-        if filename.endswith(".mhd") and count < 20:
+        if filename.endswith(".mhd") and count < 2:
             count += 1
             femur_id: str = filename[0:7]
 
-            femur_surface = FemurSurface(data_dir, filename)
+            femur_surface = FemurSurface(data_dir, filename, downsampling_ratio)
             femur_surface.generate_smoothed_surface()
             femur_surface.downsample_vertices()
             if count == 1 and visualise:
                 femur_surface.visualise_surface()
 
-            # NUMPY ARRAY
-            points = femur_surface.get_surface().points()
-
-            # VTK Follower - Vedo Points
-            vedo_points = Points(femur_surface.get_surface().points())
-            
+            pointcloud = femur_surface.get_surface().points()
             os.chdir(pointcloud_dir)
-            io.write(Points(femur_surface.get_surface().points()), file_header + "cloud_" + femur_id + ".ply")
+            pointcloud.tofile(file_header + "cloud_" + femur_id)
 
 
 def main(argv):
@@ -138,13 +146,16 @@ def main(argv):
     args = __parse_arguments()
     data_dir: str = args.segmented_data_dir
     visualise: bool = args.visualise
+    pointcloud_dir: str = args.pointcloud_data_dir
+    downsampling_ratio: float = args.downsampling_ratio
 
     data_dir = "/home/aitor/src/FemurPrediction/OAI-Data"
     pointcloud_dir = "/home/aitor/src/FemurPrediction/SmoothPointclouds"
     visualise = False
+    downsampling_ratio = 0.25
 
-    load_femur_bone(data_dir, visualise, pointcloud_dir)
+    load_femur_bone(data_dir, visualise, pointcloud_dir, downsampling_ratio)
 
 
 if __name__ == "__main__":
-    main(sys.argv)
+    sys.exit(main(sys.argv))
